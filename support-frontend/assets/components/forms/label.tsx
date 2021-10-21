1b03252fc4b161fd470487c4e4e676bd248a5ab5
George Haberis <george.haberis@guardian.co.uk>
2021-10-21 13:11:35 +0100

    fix TS and ESLint errors in assets/components/forms/label.tsx

--- support-frontend/assets/components/forms/label.tsx
+++ support-frontend/assets/components/forms/label.tsx
@@ -1,30 +1,37 @@
 // ----- Imports ----- //
-import type { Node } from 'react';
+import type { ReactElement, ReactNode } from 'react';
 import React from 'react';
 import type { Option } from 'helpers/types/option';
-import 'helpers/types/option';
 import './label.scss';
+
 // ----- Types ----- //
 export type PropsForHoc = {
 	label: string;
 	optional?: boolean;
-	footer?: Node;
+	footer?: ReactNode;
 	labelId?: string;
 };
 type Props = PropsForHoc & {
 	htmlFor: Option<string>;
-	children: Node;
+	children: ReactNode;
 };
 
 // ----- Component ----- //
-function Label({ label, children, footer, htmlFor, optional, labelId }: Props) {
+function Label({
+	label,
+	children,
+	footer,
+	htmlFor,
+	optional,
+	labelId,
+}: Props): ReactElement {
 	const Element = htmlFor ? 'label' : 'strong';
 	return (
 		<div className="component-form-label">
 			<Element
 				className="component-form-label__label"
 				id={labelId}
-				htmlFor={htmlFor}
+				htmlFor={htmlFor ? htmlFor : undefined}
 			>
 				{label}
 				{optional && (
